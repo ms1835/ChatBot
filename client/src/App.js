9fc625ad6d1be1b96ba7b838299d0d46c0d50bbf
ms1835 <mayanksingh5399@gmail.com>
2024-03-16 17:55:07 +0530

    Updated protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -7,39 +7,39 @@ const App = () => {
   const ProtectedRoute = ({children, auth=false}) => {
     const isLoggedIn = localStorage.getItem('user:token') != null;
 
-    // if(!isLoggedIn && auth){
-    //   return <Navigate to={'/api/login'} />
-    // }
-    // else if(isLoggedIn && ['/api/login', '/api/register'].includes(window.location.pathname)){
-    //   return <Navigate to={'/'} />
-    // }
+    if(!isLoggedIn && auth){
+      return <Navigate to={'/api/login'} />
+    }
+    else if(isLoggedIn && ['/api/login', '/api/register'].includes(window.location.pathname)){
+      return <Navigate to={'/'} />
+    }
 
     return children;
   }
 
   return (
-    <Routes>
-      <Route path='/api/login' element={<SignIn />} />
-      <Route path='/api/register' element={<SignUp />} />
-      <Route path='/' element={<Dashboard />} />
-    </Routes>
     // <Routes>
-    //   <Route path="/api/login" element={
-    //     <ProtectedRoute>
-    //       <SignIn />
-    //     </ProtectedRoute>
-    //   } />
-    //   <Route path="/api/register" element={
-    //     <ProtectedRoute>
-    //      <SignUp />
-    //     </ProtectedRoute>
-    //   }/>
-    //   <Route path='/' element={
-    //     <ProtectedRoute auth={true}>
-    //       <Dashboard />
-    //     </ProtectedRoute>
-    //   } />
+    //   <Route path='/api/login' element={<SignIn />} />
+    //   <Route path='/api/register' element={<SignUp />} />
+    //   <Route path='/' element={<Dashboard />} />
     // </Routes>
+    <Routes>
+      <Route path="/api/login" element={
+        <ProtectedRoute>
+          <SignIn />
+        </ProtectedRoute>
+      } />
+      <Route path="/api/register" element={
+        <ProtectedRoute>
+         <SignUp />
+        </ProtectedRoute>
+      }/>
+      <Route path='/' element={
+        <ProtectedRoute auth={true}>
+          <Dashboard />
+        </ProtectedRoute>
+      } />
+    </Routes>
   )
 }
 
